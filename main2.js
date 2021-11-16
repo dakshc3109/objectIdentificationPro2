22b5b810affea36f8b0749c2eb464d16e7dda5df
Daksh Chaudhary <78908644+dakshc3109@users.noreply.github.com>
2021-11-16 11:52:35 +0530

    //done

---
+++ main2.js
@@ -0,0 +1,33 @@
+var status = "";
+object = [];
+
+function preload(){
+    img = loadImage("tv&ac.jpg")
+}
+
+function setup(){
+    canvas = createCanvas(640, 400);
+    canvas.position(330, 150);;
+    objetDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting object";
+}
+
+function modelLoaded(){
+   console.log("model laoded");
+   status = true;
+   objetDetector.detect(img, gotResult); 
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error();
+    }
+    else{
+        object = results;
+        console.log(object);
+    }
+}
